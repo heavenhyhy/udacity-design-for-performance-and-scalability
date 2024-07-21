8ab66c96d38b4f6a30854aacd56e85f819a45912
sangtran <sang56pm@gmail.com>
2024-07-21 18:22:59 +0700

    add cloudwatch log permission

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -33,6 +33,14 @@ resource "aws_iam_role" "udacity_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+// can not directly modify assume_role due to lack of permission so we need to attach it separate
+resource "aws_iam_role_policy_attachment" "cloud_watch_logs" {
+  depends_on = [aws_iam_role.udacity_lambda]
+
+  role       = aws_iam_role.udacity_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = var.lambda_file
@@ -43,15 +51,15 @@ resource "aws_lambda_function" "greet" {
   filename      = data.archive_file.lambda.output_path
   function_name = var.function_name
   role          = aws_iam_role.udacity_lambda.arn
-  handler       = var.lambda_handler
+  handler       = "${var.function_name}.${var.lambda_handler}"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  runtime = "nodejs18.x"
+  runtime = "python3.10"
 
   environment {
     variables = {
-      tag = "Udacity"
+      greeting = "Udacity SangTD2"
     }
   }
 }
